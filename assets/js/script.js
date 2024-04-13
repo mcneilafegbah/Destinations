5ead9f86722e19b761c14557b9236b1bef246f6c
mcneilafegbah <mcneilko@gmail.com>
2024-04-13 20:06:53 +0000

    Created a banner with text and 5 destination button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@
  * navbar toggle
  */
 
-const overlay = document.querySelector("[data-overlay]");
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[menu-overlay]");
+const navOpenBtn = document.querySelector("[menu-nav-btn]");
+const navbar = document.querySelector("[menu-navbar]");
+const navCloseBtn = document.querySelector("[menu-nav-close-btn]");
+const navLinks = document.querySelectorAll("[menu-nav-link]");
 
 const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
